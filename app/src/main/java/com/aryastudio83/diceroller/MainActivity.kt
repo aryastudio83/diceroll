1d27a4d3a1d259c8c8670e2ce341aa59357c6182
aryastudio <aryastudio83@gmail.com>
2022-04-16 23:12:11 +0700

    add dice images

--- app/src/main/java/com/aryastudio83/diceroller/MainActivity.kt
+++ app/src/main/java/com/aryastudio83/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aryastudio83.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,10 +16,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+      val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 

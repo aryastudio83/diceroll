82ca5b11a2d5021ea3aa81a6dee8d40f5fd42dd1
aryastudio <aryastudio83@gmail.com>
2022-04-18 21:22:15 +0700

    Add images

--- app/src/main/java/com/aryastudio83/diceroller/MainActivity.kt
+++ app/src/main/java/com/aryastudio83/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package com.aryastudio83.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,12 +11,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-      val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val dice = Dice(6)
+        val drawableRes = when (dice.roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableRes)
+        diceImage.contentDescription = dice.roll().toString()
+
     }
 }
 
